7dff01424c4f7ec0d57045f9f8ac2482039be2de
Evan Willhite <evan@fourkitchens.com>
2020-05-07 17:51:23 -0500

    404

---
+++ src/pages/404.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+import { Helmet } from 'react-helmet'
+
+import styles from '../components/pages/home.module.css'
+
+import Layout from '../components/layout'
+import ArticlePreview from '../components/molecules/teasers/article'
+import SermonPreview from '../components/molecules/teasers/sermon'
+
+class RootIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+    const sermons = get(this, 'props.data.allContentfulSermon.edges')
+
+    return (
+      <Layout location={this.props.location}>
+        <div style={{ background: '#fff' }}>
+          <Helmet title={siteTitle}>
+            <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400&family=Poppins:wght@300&display=swap" rel="stylesheet" />
+          </Helmet>
+            <section className="wrapper">
+              <p>Sorry, the page you are looking for is not found.</p>
+              <br></br>
+              <Link className="button" to="/">Return to the Homepage</Link>
+            </section>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default RootIndex
+
+export const pageQuery = graphql`
+  query pageNotFoundQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
