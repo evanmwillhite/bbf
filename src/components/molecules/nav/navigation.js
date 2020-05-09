950e892d02467689153aede77ed5ace9fdcf70c8
Evan Willhite <evan@fourkitchens.com>
2020-05-09 08:53:38 -0500

    movile nav wip

--- src/components/molecules/nav/navigation.js
+++ src/components/molecules/nav/navigation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import styles from './navigation.module.css'
@@ -9,53 +9,73 @@ import Close from '../../../img/close.inline.svg';
 import classNames from 'classnames/bind';
 let cx = classNames.bind(styles);
 
-export default ({ light }) => {
-  let navLinkClasses = cx({
-    navlink: true,
-    navlinkHome: light,
-  });
+export default class Nav extends Component {
+  state = { revealed: false };
 
-  return (
-    <nav role="navigation" className={styles.nav}>
-      <button id="toggle-expand" class={styles.toggleExpand}>
-        <span class={styles.toggleExpand__open}>
-          <Menu />
-          <span class={styles.toggleExpand__text}>Main Menu</span>
-        </span>
-        <span class={styles.toggleExpand__close}>
-          <Close />
-          <span class={styles.toggleExpand__text}>Close</span>
-        </span>
-      </button>
-      <div id="main-nav" class={styles.mainNav}>
-        <ul className={styles.navigation}>
-          {!light && 
+  reveal = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      revealed: !prevState.revealed
+    }));
+  };
+
+  render() {
+    const { light } = this.props;
+    let navLinkClasses = cx({
+      navlink: true,
+      navlinkHome: light,
+    });
+
+    const toggleOpenClasses = cx({
+      toggleExpand__open: true,
+      closed: this.state.revealed === true
+    });
+
+    const toggleCloseClasses = cx({
+      toggleExpand__close: true,
+      open: this.state.revealed === true
+    });
+
+    return (
+      <nav role="navigation" className={styles.nav}>
+        <button id="toggle-expand" class={styles.toggleExpand}>
+          <span class={toggleOpenClasses}>
+            <Menu onClick={this.reveal} />
+          </span>
+          <span class={toggleCloseClasses}>
+            <Close onClick={this.reveal} />
+          </span>
+        </button>
+        <div id="main-nav" class={styles.mainNav}>
+          <ul className={styles.navigation}>
+            {!light && 
+              <li className={styles.navigationItem}>
+                <Link className={navLinkClasses} to="/">Home</Link>
+              </li>
+            }
             <li className={styles.navigationItem}>
-              <Link className={navLinkClasses} to="/">Home</Link>
+              <Link className={navLinkClasses} to="/visit/">Visit</Link>
             </li>
-          }
-          <li className={styles.navigationItem}>
-            <Link className={navLinkClasses} to="/visit/">Visit</Link>
-          </li>
-          <li className={styles.navigationItem}>
-            <Link className={navLinkClasses} to="/about/">About</Link>
-            <ul className={styles.subNav}>
-              <li>
-                <Link className={navLinkClasses} to="/ministries/">Ministries</Link>
-              </li>
-              <li>
-                <Link className={navLinkClasses} to="/history/">History</Link>
-              </li>
-              <li>
-                <Link className={navLinkClasses} to="/staff/">Staff</Link>
-              </li>
-            </ul>
-          </li>
-          <li className={styles.navigationItem}>
-            <Link className={navLinkClasses} to="/inspiration/">Inspiration</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+            <li className={styles.navigationItem}>
+              <Link className={navLinkClasses} to="/about/">About</Link>
+              <ul className={styles.subNav}>
+                <li>
+                  <Link className={navLinkClasses} to="/ministries/">Ministries</Link>
+                </li>
+                <li>
+                  <Link className={navLinkClasses} to="/history/">History</Link>
+                </li>
+                <li>
+                  <Link className={navLinkClasses} to="/staff/">Staff</Link>
+                </li>
+              </ul>
+            </li>
+            <li className={styles.navigationItem}>
+              <Link className={navLinkClasses} to="/inspiration/">Inspiration</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
