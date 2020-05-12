c3a956ecb231ef49f780e8d1c0e8b54147dd5d0d
Evan Willhite <evan@fourkitchens.com>
2020-05-12 16:12:17 -0500

    site url fix

--- src/components/base/seo/seo.js
+++ src/components/base/seo/seo.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
+import { useLocation } from "@reach/router"
 import { useStaticQuery, graphql } from "gatsby"
 
 import favicon from '../../../img/favicon.png';
