ffd6c2a8702ef4f2f2cc9d4b067506bd4717c9cb
Evan Willhite <evan@fourkitchens.com>
2020-05-11 23:51:28 -0500

    simplified cropping

--- src/templates/page.js
+++ src/templates/page.js
@@ -183,7 +183,7 @@ export const pageQuery = graphql`
           title
           shortDescription
           image {
-            fluid(maxWidth: 500, maxHeight: 600, resizingBehavior: CROP) {
+            fluid(maxWidth: 500, maxHeight: 600) {
               ...GatsbyContentfulFluid_tracedSVG
             }
           }
