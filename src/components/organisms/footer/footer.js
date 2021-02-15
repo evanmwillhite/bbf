3c281f677de2dddce46b4277dc5f8acef823c8ec
Evan Willhite <evan@fourkitchens.com>
2021-02-15 16:20:19 -0600

    add physical address in footer

--- src/components/organisms/footer/footer.js
+++ src/components/organisms/footer/footer.js
@@ -9,6 +9,12 @@ import styles from './footer.module.css'
 export default () => (
   <footer className={styles.footer}>
     <div className={styles.footerWrap + ' wrapper'}>
+      <address className={styles.address}>
+        <span>Believers Baptist Fellowship</span>
+        <span>260 West Main Street</span>
+        <span>Suite 103</span>
+        <span>Hendersonville, Tennessee 37075</span>
+      </address>
       <ul className={styles.footerMenu}>
         <li className={styles.footerListItem}>
           <a className={styles.facebook} target="_blank" href="https://www.facebook.com/believersbaptistfellowship/" rel="noopener">
@@ -21,7 +27,7 @@ export default () => (
           </a>
         </li>
       </ul>
-      <p><span>Copyright © {(new Date().getFullYear())} Believers Baptist Fellowship - All Rights Reserved.</span><span><a href="tel:+1-615-517-8800">(615) 517-8800</a></span><span><a href="mailto:akowillhite@gmail.com">akowillhite@gmail.com</a></span></p>
+      <p><span className={styles.copyright}>Copyright © {(new Date().getFullYear())} Believers Baptist Fellowship - All Rights Reserved.</span><span className={styles.copyright}><a href="tel:+1-615-517-8800">(615) 517-8800</a></span><span className={styles.copyright}><a href="mailto:akowillhite@gmail.com">akowillhite@gmail.com</a></span></p>
     </div>
   </footer>
 )
